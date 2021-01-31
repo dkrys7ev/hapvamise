baf8cf277f3560cc139e0e3f52bb2bc6b1068b2b
Danislav Krastev <dkrystev.2create@gmail.com>
2021-01-31 12:46:55 +0200

    Add search filters

--- src/ajax/recipe.php
+++ src/ajax/recipe.php
@@ -1,10 +1,44 @@
 <?php
+$type    = $_POST['type'];
+$product = $_POST['product'];
 
-$type                = $_POST['type'];
-$json                = file_get_contents("recipes/{$type}/data.json");
-$recipes             = json_decode( $json )->recipes;
+if ( ! isset( $type ) || empty( $type ) ) {
+	echo json_encode( array(
+		'success' => false,
+		'recipe'  => array(),
+	) );
+}
+
+$json    = file_get_contents("recipes/{$type}/data.json");
+$recipes = json_decode( $json )->recipes;
+
+if ( isset( $product ) && ! empty( $product ) ) {
+	$recipes = array_filter($recipes, "hpv_filter_recipes");
+}
+
+$recipes             = array_values( $recipes );
+
+if ( empty( $recipes ) ) {
+	echo json_encode( array(
+		'success' => false,
+		'recipe'  => array(),
+	) );
+
+	exit;
+}
+
 $random_recipe_index = array_rand( $recipes, 1 );
 $recipe              = $recipes[ $random_recipe_index ];
 
-echo json_encode($recipe);
+echo json_encode( array(
+	'success' => true,
+	'recipe'  => $recipe,
+) );
+
 exit;
+
+function hpv_filter_recipes( $recipe ) {
+	$product = $_POST['product'];
+
+	return in_array( $product, $recipe->keywords );
+}
--- src/assets/js/modules/pick-recipe.js
+++ src/assets/js/modules/pick-recipe.js
@@ -1,39 +1,52 @@
 import { $doc, $body } from "../utils/globals.js";
 
 const $recipe = $(".recipe");
+const $error = $(".error");
 const $recipeIngredients = $recipe.find(".recipe__ingredients");
 const $recipePreparation = $recipe.find(".recipe__preparation");
 
 $doc.on("submit", ".form-recipe", function (event) {
 	event.preventDefault();
 
+	const $form = $(this);
+	const formData = $form.serializeArray();
+
 	$.ajax({
 		method: "POST",
 		url: "ajax/recipe.php",
-		data: {
-			type: "veggie",
-			// keywords: ["peas", "potatoes"]
-		},
+		data: formData,
 		beforeSend: function () {
 			$body.addClass("loading");
 			$recipe.addClass("hidden");
-			$recipeIngredients.html('');
+			$error.addClass("hidden");
+			$recipeIngredients.html("");
 		},
 		success: function (response) {
-			const randomRecipe = JSON.parse(response);
-			const randomRecipeIngredients = randomRecipe.ingredients;
+			response = JSON.parse(response);
 
-			$recipe.find(".recipe__title").html(randomRecipe.name);
-			$recipe.find(".recipe__image img").attr("src", randomRecipe.image);
-			$recipe.find(".recipe__preparation").html(randomRecipe.preparation);
+			if (response.success) {
+				const randomRecipe = response.recipe;
+				const randomRecipeIngredients = randomRecipe.ingredients;
 
-			randomRecipeIngredients.forEach(function (ingredient) {
-				$recipeIngredients.append("<li>" + ingredient + "</li>");
-			});
+				$recipe.find(".recipe__title").html(randomRecipe.name);
+				$recipe
+					.find(".recipe__image img")
+					.attr("src", randomRecipe.image);
+				$recipe
+					.find(".recipe__preparation")
+					.html(randomRecipe.preparation);
+
+				randomRecipeIngredients.forEach(function (ingredient) {
+					$recipeIngredients.append("<li>" + ingredient + "</li>");
+				});
+
+				$recipe.removeClass("hidden");
+			} else {
+				$error.removeClass("hidden");
+			}
 		},
 		complete: function (response) {
 			$body.removeClass("loading");
-			$recipe.removeClass("hidden");
 		},
 	});
 });
